51f3885dc5e2575b48d20c8cbd94d4dc34fef18d
Milor <milorcete@gmail.com>
2025-10-30 15:12:33 +0100

    Actualitzat l'estat 'Actiu' dels articles a la base de dades: articles 5 i 9 ara són inactius.

---
+++ ComandesAPI/Migrations/20251030140259_UpdateArticlesActiu.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComandesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateArticlesActiu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Actiu",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Actiu",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Actiu",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Actiu",
+                value: true);
+        }
+    }
+}
